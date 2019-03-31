e62c2c781b9c3f2e279ec7acbffe23d1b94c7e15
Paul Littlebury <paullittlebury@gmail.com>
2019-04-01 01:21:48 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-
+import webdriver from 'w3c-webdriver';
+let session;
+(async () => {
+  try {
+    session = await webdriver.newSession('http://localhost:4444', {
+      desiredCapabilities: {
+        browserName: 'Chrome',
+        chromeOptions: {
+          args: ["headless", "disable-gpu"]
+        }
+      }
+    });
+    await session.go('http://your-netlify-web-address');
+    const element = await session.findElement('css selector', 'a');
+    await element.click();
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    session.deleteSession();
+  }
+})();
