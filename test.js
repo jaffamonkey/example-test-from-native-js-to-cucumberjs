c5a05037ec12e75d7f8fcca1c819d200fa0c124a
jaffamonkey <paullittlebury@gmail.com>
2019-04-01 19:19:56 +0200

    updates

--- test.js
+++ test.js
@@ -16,8 +16,12 @@ let session;
     const input = await session.findElement('css selector', '[name="q"]');
     console.log('Found element (Ok)');
     await input.sendKeys('donald trump simulator');
-    console.log('Value entered in search field and submitted (Ok)');
-    const result = await session.findElement('css selector', '.repo-list');
+    console.log('Value entered in search field (Ok)');
+    const button = await session.findElement('css selector', '[name="search"]');
+    console.log('Found element (Ok)');
+    await button.click();
+    const result = await session.findElement('css selector', 'p');
+    console.log('Search results page loaded (Ok)');
     const text = await result.getText();
     console.log(text)
   } catch (err) {
