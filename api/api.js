5b948ef0e81b648c04bab511ba16c9ccc47d4381
jaffamonkey <paullittlebury@gmail.com>
2019-04-04 04:06:26 +0200

    updates

--- api/api.js
+++ api/api.js
@@ -1,8 +1,44 @@
 var express = require("express");
-var app = express();
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+var db = require("./db/db,js");
+var bodyParser = require("body-parser");
+
+// Set up the express app
+const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db
+  })
 });
-app.get("/test-get", (req, res, next) => {
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+
+app.post('/api/v1/todos', (req, res) => {
+  if(!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    });
+  }
+ const todo = {
+   id: db.length + 1,
+   title: req.body.title,
+ }
+ db.push(todo);
+ return res.status(201).send({
+   success: 'true',
+   message: 'todo added successfully',
+   todo
+ })
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
 });
--- docs/API.md
+++ docs/API.md
@@ -3,3 +3,5 @@ The simplest API call is retrieving a web page, which returns a status code of 2
 But there is a lot you can do at API level.
 
 There is a most API server code in the `api` folder, as a demontsration of both api server and test.
+
+The test is short and sweet, 
