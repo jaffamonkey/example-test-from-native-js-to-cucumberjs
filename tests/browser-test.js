67a580110ec52503df0de6fa7aefe45f02e19d1c
jaffamonkey <paullittlebury@gmail.com>
2019-04-06 00:17:20 +0200

    updates

--- tests/browser-test.js
+++ tests/browser-test.js
@@ -3,7 +3,7 @@ var webdriver = require('selenium-webdriver'),
   By = webdriver.By,
   until = webdriver.until;
 
-driver = new webdriver
+browser = new webdriver
   .Builder()
   .usingServer()
   .withCapabilities({
@@ -13,24 +13,24 @@ driver = new webdriver
     }
   }).build();
 
-driver.get('http://localhost:8081');
+browser.get('http://localhost:8081');
 
 // waiting for element to load, the fill in field
-driver.wait(until.elementLocated(By.name('q')), 10000, 'Could not locate').sendKeys('donald trump simulator');
+browser.wait(until.elementLocated(By.name('q')), 10000, 'Could not locate').sendKeys('donald trump simulator');
 
 // Locating the element that has name "search", then click
-driver.wait(until.elementLocated(By.name('search')), 10000, 'Could not locate').click();
+browser.wait(until.elementLocated(By.name('search')), 10000, 'Could not locate').click();
 
 // waiting for search results to load
-driver.wait(until.elementLocated(By.css('.result__snippet')), 10000, 'Could not locate');
+browser.wait(until.elementLocated(By.css('.result__snippet')), 10000, 'Could not locate');
 
 // Verifying the search results page title
-driver.getTitle().then(function (title) {
+browser.getTitle().then(function (title) {
   if (title === 'donald trump simulator site:github.com at DuckDuckGo') {
     console.log('The title "' + title + '" is correct');
   } else {
     console.log('The title "' + title + '" is incorrect');
   }
   // Close web session
-  driver.quit();
+  browser.quit();
 });
