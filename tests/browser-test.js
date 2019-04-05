cbea7f6c8da7e570b29ee9ee52427c3d4d571611
jaffamonkey <paullittlebury@gmail.com>
2019-04-05 06:31:06 +0200

    updates

--- tests/browser-test.js
+++ tests/browser-test.js
@@ -1,8 +1,12 @@
 var webdriver = require('w3c-webdriver');
 
-let session;
+
+// Mainly useful for development, but for the test "async" provide 
+// useful way to makde tests run smoothly, by reducing time between the steps.
 (async () => {
   try {
+
+    // This part starts up webdriver and specifies the browser options
     session = await webdriver.newSession('http://localhost:9515', {
       desiredCapabilities: {
         browserName: 'Chrome',
@@ -11,28 +15,51 @@ let session;
         }
       }
     });
+
+    // This is the point the browser first opens
     await session.go('https://elated-montalcini-28a317.netlify.com');
+
+    // console.log is standard Javascript to output text to the screen.
+    // Is this context, it's outputting confirmation message step passed ok.
     console.log('Opening the homepage (Ok)');
+
+    // Locating the field that has name "q" 
+    // "await" tells the test to wait until element is visible (can timeout)
     const input = await session.findElement('css selector', '[name="q"]');
     console.log('Found element (Ok)');
+
+    // Fill in field "q" with the text within the brackets
     await input.sendKeys('donald trump simulator');
     console.log('Value entered in search field (Ok)');
+
+    // Locating the field that has name "search" 
     const button = await session.findElement('css selector', '[name="search"]');
     console.log('Found element (Ok)');
+
+    // Click on the "search" button
     await button.click();
     const result = await session.findElement('css selector', '.result__snippet');
     const text = await result.getText();
     console.log('Result output (Ok)');
+
+    // The line below will return a value of True or False.
+    // This depends on whether the results text contains the expected value.
     const check = text.includes('Donald Trump');
-    if (check === true) 
-    {
+
+    // A basic if-else function - the 3 "=" are necessary when checking boolean value
+    if (check === true) {
       console.log('The actual results match the expected results')
     }
-    else
-    {
+    else {
       console.log('The actual results does not match the expected results')
     }
-  } catch (err) {
+  }
+  // When a step fails, the test immediately exit and this function outputs error detail to the screen.
+  catch (err) {
     console.log(err.stack);
+  }
+  // Stop Chromedriver and Browser
+  finally {
+    session.deleteSession;
   }
 })();
