7c09c8a9ff6e029aeba091ada3223f85c2e38d94
jaffamonkey <paullittlebury@gmail.com>
2019-04-06 00:54:20 +0200

    updates

--- web/server.js
+++ web/server.js
@@ -6,33 +6,36 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
-http.createServer(function(req, res){
+http.createServer(function (req, res) {
 
-   // If the url path is root of domain 
-    if(req.url === "/"){
-       // Open the "index.html" file
-        fs.readFile("index.html", "UTF-8", function(err, html){
-           // Include the contebnt type in the HTML <head> section
-            res.writeHead(200, {"Content-Type": "text/html"});
+    // If the url path is root of domain ...
+    if (req.url === "/") {
+
+        // ... open the "index.html" file ...
+        fs.readFile("index.html", "UTF-8", function (err, html) {
+
+            // ... then include the relevant content type in the HTML <head> section
+            res.writeHead(200, { "Content-Type": "text/html" });
             res.end(html);
         });
     }
     // For url's that has ".png" in url, i.e. am image location
-    else if(req.url.match("\.png$")){
-       // Define general images path
+    else if (req.url.match("\.png$")) {
+
+        // Define general images path
         var imagePath = path.join(__dirname, 'images', req.url);
         var fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, {"Content-Type": "image/png"});
+        res.writeHead(200, { "Content-Type": "image/png" });
         fileStream.pipe(res);
     }
-    
+
     else if (req.url == '/data') { //check the URL of the current request
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.write(JSON.stringify({ message: "Hello World"}));  
-      res.end();  
-   }  
-   else{
-        res.writeHead(404, {"Content-Type": "text/html"});
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: "Hello World" }));
+        res.end();
+    }
+    else {
+        res.writeHead(404, { "Content-Type": "text/html" });
         res.end("No Page Found");
     }
 
