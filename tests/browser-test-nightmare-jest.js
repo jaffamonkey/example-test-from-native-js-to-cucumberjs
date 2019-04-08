b582e6bafa1f19ced92203edfff13b1e49b100d6
jaffamonkey <paullittlebury@gmail.com>
2019-04-08 04:39:34 +0200

    updates

--- tests/browser-test-nightmare-jest.js
+++ tests/browser-test-nightmare-jest.js
@@ -2,22 +2,22 @@
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true })
 
-describe('Simple website example', function () {
-  test('test the search', async (done) => {
-//      let page = nightmare().goto('http://localhost:8081')
-//      let text = await page.evaluate(() => document.body.textContent)
-//                          .end()
-//      expect(text).toContain('Welcome to React')
+describe('Simple website example', () => {
+  test('test the search',  async function () {
      await nightmare
       .goto('http://localhost:8081')
       .type('input[name="q"]', 'donald trump simulator')
       .click('#searchButton')
-      .evaluate(function () {
-        document.getElementsByTagName('body').innerText;
-      })
+      .evaluate(function() {
+        return document.querySelector('.results--main').innerText
+       })
       .end()
-      .then(function (text) {
-        expect(text).toContain('Donald Trump');
+      .then(function(text) {
+      console.log(text);
+      expect(text).toContain('Donald Trump');
       })
+      .catch(function(error) {
+      console.error('Search failed:', error)
+    })
   });
 });
