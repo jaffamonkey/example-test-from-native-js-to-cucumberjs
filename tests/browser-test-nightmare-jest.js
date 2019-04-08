b2928ea61227f46a8d55ddbbebfb9fe9006fb5a9
Paul Littlebury <paullittlebury@gmail.com>
2019-04-08 04:01:59 +0200

    Update browser-test-nightmare-jest.js

--- tests/browser-test-nightmare-jest.js
+++ tests/browser-test-nightmare-jest.js
@@ -4,7 +4,11 @@ const nightmare = Nightmare({ show: true })
 
 describe('Simple website example', function () {
   test('test the search', async (done) => {
-    await nightmare
+//      let page = nightmare().goto('http://localhost:8081')
+//      let text = await page.evaluate(() => document.body.textContent)
+//                          .end()
+//      expect(text).toContain('Welcome to React')
+     await nightmare
       .goto('http://localhost:8081')
       .type('input[name="q"]', 'donald trump simulator')
       .click('#searchButton')
