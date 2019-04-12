a3ab0a0e0308eec307554753346b45fbdfaeabe8
jaffamonkey <paullittlebury@gmail.com>
2019-04-12 14:37:53 +0200

    updates

--- tests/api-test.js
+++ tests/api-test.js
@@ -1,24 +1,27 @@
-const superagent = require('superagent');
-// Generates a title of 10 characters, with random letters, each time test is run
+const request = require("request");
 let randomTitle = Math.random().toString(36).substring(10);
 
-superagent
-  .post('http://localhost:3001/api/v1/todos')
-  .send({ "title": randomTitle })
-  .end(function (err, res) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(res.body)
-    }
-  });
+const get_options = {
+  url: 'http://localhost:3001/api/v1/todos',
+  method: 'GET'
+};
 
-  superagent
-  .get('http://localhost:3001/api/v1/todos')
-  .end(function (err, res) {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(res.body)
-    }
-  });
+const post_options = {
+  url: 'http://localhost:3001/api/v1/todos',
+  method: 'POST',
+  form: { title: randomTitle }
+};
+
+request(post_options, function (err, res, body) {
+  let json = JSON.parse(body);
+  console.log(json);
+  console.log('Response Code: ' + res.statusCode);
+});
+
+
+request(get_options, function (err, res, body) {
+  let json = JSON.parse(body);
+  console.log(json);
+  console.log('Response Code: ' + res.statusCode);
+});
+
