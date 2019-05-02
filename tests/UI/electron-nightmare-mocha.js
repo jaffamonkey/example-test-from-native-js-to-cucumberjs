92e8db7ba4bed154f24ea1e74d40f3cc7e41e7ac
jaffamonkey <paullittlebury@gmail.com>
2019-05-02 13:07:11 +0200

    updates

---
+++ tests/UI/electron-nightmare-mocha.js
@@ -0,0 +1,66 @@
+const Nightmare = require('nightmare');
+const assert = require('assert');
+const resultsarea = '.results';
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Check the DuckDuckGo search results page title', function () {
+  this.timeout('60s')
+
+  let nightmare = null
+  beforeEach(() => {
+    nightmare = new Nightmare({ show: true })
+  })
+
+  describe('Homepage', function () {
+    describe('Page Title', () => {
+      it('should show correct results page title', done => {
+        // your actual testing urls will likely be `http://localhost:port/path`
+        nightmare.goto('http://localhost:8081')
+          .type('input[name="q"]', 'donald trump simulator')
+          .click('#searchButton')
+          .wait(3000)
+          .evaluate(() => document.getElementsByTagName('title')[0].innerText)
+          .then(function (title) {
+            expect(title).contains('donald trump simulator site:github.com at DuckDuckGo')
+            done()
+          })
+          .catch(done)
+          .then(_ => {
+            // finally cleanup
+            nightmare.end();
+            // kill the Electron process explicitly to ensure no orphan child processes
+            nightmare.proc.disconnect();
+            nightmare.proc.kill();
+            nightmare.ended = true;
+            nightmare = null;
+          });
+      })
+    })
+
+    describe('Search Results', () => {
+      it('should show correct results', done => {
+        nightmare
+          .goto('http://localhost:8081')
+          .type('input[name="q"]', 'donald trump simulator')
+          .click('#searchButton')
+          .wait(3000)
+          .evaluate(() => document.getElementsByClassName('result__body')[0].innerText)
+          .then(function (text) {
+            expect(text).contains('TrumpKlon')
+            done()
+          })
+          .catch(done)
+          .then(_ => {
+            // finally cleanup
+            nightmare.end();
+            // kill the Electron process explicitly to ensure no orphan child processes
+            nightmare.proc.disconnect();
+            nightmare.proc.kill();
+            nightmare.ended = true;
+            nightmare = null;
+          });
+      })
+    })
+  })
+})
