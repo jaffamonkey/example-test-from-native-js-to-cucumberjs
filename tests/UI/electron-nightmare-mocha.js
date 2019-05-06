cbeffda52529e9b9512a0e883b23f77c56d48927
jaffamonkey <paullittlebury@gmail.com>
2019-05-06 19:36:08 +0200

    updates

--- tests/UI/electron-nightmare-mocha.js
+++ tests/UI/electron-nightmare-mocha.js
@@ -19,46 +19,28 @@ describe('Check the DuckDuckGo search results page title', function () {
           .click('#searchButton')
           .wait(3000)
           .evaluate(() => document.getElementsByTagName('title')[0].innerText)
+          .end()
           .then(function (title) {
-            expect(title).contains('donald trump simulator site:github.com at DuckDuckGo')
+            expect(title).contains('donald trump simulator site:github.com at DuckDuckGo');
             done()
           })
-          .catch(done)
-          .then(_ => {
-            // finally cleanup
-            nightmare.end();
-            // kill the Electron process explicitly to ensure no orphan child processes
-            nightmare.proc.disconnect();
-            nightmare.proc.kill();
-            nightmare.ended = true;
-            nightmare = null;
-          });
       })
     })
+  })
 
-    describe('Search Results', () => {
-      it('should show correct results', done => {
-        nightmare
-          .goto('http://localhost:8081')
-          .type('input[name="q"]', 'donald trump simulator')
-          .click('#searchButton')
-          .wait(3000)
-          .evaluate(() => document.getElementsByClassName('result__body')[0].innerText)
-          .then(function (text) {
-            expect(text).contains('Donald Trump')
-            done()
-          })
-          .catch(done)
-          .then(_ => {
-            // finally cleanup
-            nightmare.end();
-            // kill the Electron process explicitly to ensure no orphan child processes
-            nightmare.proc.disconnect();
-            nightmare.proc.kill();
-            nightmare.ended = true;
-            nightmare = null;
-          });
-      })
+  describe('Search Results', () => {
+    it('should show correct results', done => {
+      nightmare
+        .goto('http://localhost:8081')
+        .type('input[name="q"]', 'donald trump simulator')
+        .click('#searchButton')
+        .wait(3000)
+        .evaluate(() => document.getElementsByClassName('result__body')[0].innerText)
+        .end()
+        .then(function (text) {
+          expect(text).contains('Donald Trump')
+          done()
+        })
     })
   })
 })
