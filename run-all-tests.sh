8ace5bb88a6d3769cc94359d58c687144c808f23
jaffamonkey <paullittlebury@mail.com>
2020-03-21 14:29:54 +0000

    test updates

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,12 +1,23 @@
 
+sleep 3
 npm run test:mocha
+sleep 3
 npm run test:nightmare
+sleep 3
 npm run test:jest
+sleep 3
 npm run test:nightwatchapi
+sleep 3
 npm run test:nightwatch
+sleep 3
 npm run test:javascript
+sleep 3
 npm run test:webdriver
+sleep 3
 npm run test:protractor
+sleep 3
 npm run test:puppeteer
+sleep 3
 npm run test:zombie
+sleep 3
 npm run test:cucumberjs
